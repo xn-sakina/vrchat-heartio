c065a2af7830c0e031861c29c4671d29e255acd6
fz6m <59400654+fz6m@users.noreply.github.com>
2025-05-19 16:53:06 +0900

    wip: I need all

--- desktop-app/src/renderer/src/App.tsx
+++ desktop-app/src/renderer/src/App.tsx
@@ -1,6 +1,11 @@
+import { Button } from '@mantine/core'
 
 export default function App() {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
-  return <div>111</div>
+  return (
+    <div>
+      <Button fullWidth>111</Button>
+    </div>
+  )
 }
