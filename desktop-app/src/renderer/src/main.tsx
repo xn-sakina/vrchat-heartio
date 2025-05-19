a6643082e38c8a46191bc4b8233016d83f2a0d7a
fz6m <59400654+fz6m@users.noreply.github.com>
2025-05-19 16:43:58 +0900

    wip: reset css

--- desktop-app/src/renderer/src/main.tsx
+++ desktop-app/src/renderer/src/main.tsx
@@ -1,11 +1,11 @@
-import './assets/main.css'
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import 'modern-normalize/modern-normalize.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 )
