6fe0d89678e79314f49b2c650e385b3576ec39d0
fz6m <59400654+fz6m@users.noreply.github.com>
2025-05-19 16:53:06 +0900

    wip: I need all

--- desktop-app/src/renderer/src/main.tsx
+++ desktop-app/src/renderer/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import 'modern-normalize/modern-normalize.css'
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <MantineProvider>
     <App />
-  </StrictMode>,
+  </MantineProvider>,
 )
