2d7c037d49d17d38f55bf29b13fa25705e04b462
fz6m <59400654+fz6m@users.noreply.github.com>
2025-05-19 17:29:04 +0900

    wip: install script

---
+++ scripts/install.sh
@@ -0,0 +1,15 @@
+set -e
+
+file_dir=$(dirname "$0")
+root_dir=$(realpath "$file_dir/..")
+
+apple_watch_dir="$root_dir/apple-watch"
+desktop_dir="$root_dir/desktop-app"
+graph_dir="$root_dir/graph"
+sdk_dir="$root_dir/sdk-v1"
+
+# install
+cd $apple_watch_dir && pnpm i
+cd $desktop_dir && pnpm i
+cd $graph_dir && pnpm i
+cd $sdk_dir && pnpm i
