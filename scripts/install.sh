f26e8c3ece4285cf21d86c58df576d847c08322c
fz6m <59400654+fz6m@users.noreply.github.com>
2025-05-24 03:32:42 +0900

    wip: rename to cli

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,10 +6,10 @@ root_dir=$(realpath "$file_dir/..")
 apple_watch_dir="$root_dir/apple-watch"
 desktop_dir="$root_dir/desktop-app"
 graph_dir="$root_dir/graph"
-sdk_dir="$root_dir/sdk-v1"
+cli_dir="$root_dir/cli"
 
 # install
 cd $apple_watch_dir && pnpm i
 cd $desktop_dir && pnpm i
 cd $graph_dir && pnpm i
-cd $sdk_dir && pnpm i
+cd $cli_dir && pnpm i
